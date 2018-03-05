2f6f03466f17f468f1d804ad65c4eecb8e1001e6
rob.a.berger@gmail.com Berger <rob.a.berger@gmail.com>
2018-03-05 12:55:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ function theBeatlesPlay(musicians, instruments) {
 
 function johnLennonFacts(facts) {
   var i = 0;
+  var x;
   while (i < facts.length) {
     var x = [];
     x = facts[i] + "!!!";
